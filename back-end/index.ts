aa79d8a2f4e02883cac49d3fb63a4fe9be3c4a9f
Harish Anantharaj <harish18092002@gmail.com>
2025-12-05 00:42:12 +0530

    refactor: redesign UI with simplified single-page converter layout

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,10 +2,7 @@ import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { UserController } from "./main";
 
-const FRONTEND_ORIGIN =
-  process.env.FRONTEND_ORIGIN ||
-  process.env.ALLOWED_ORIGIN ||
-  "http://localhost:4200";
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || "http://localhost:4200";
 
 const app = new Elysia()
   .use(
@@ -16,16 +13,6 @@ const app = new Elysia()
       credentials: false,
     })
   )
-  .onRequest(({ set }) => {
-    // Ensure CORS headers are present even if middleware misses an edge case.
-
-    set.headers["Access-Control-Allow-Origin"] = FRONTEND_ORIGIN;
-    set.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS";
-    set.headers["Access-Control-Allow-Headers"] = "Content-Type,Accept";
-  })
-  .options("*", ({ set }) => {
-    set.status = 204;
-  })
   .use(UserController);
 
 const server = app.listen(3456);
